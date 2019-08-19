ca81c931432cba85c833d8129039cc670b8be631
LadyKerr <kadiekerr@gmail.com>
2019-08-19 12:32:08 -0400

    working on reducer store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import rootReducer from "./reducers/rootReducer";
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
+import fbConfig from "./config/fbConfig";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+/*
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+
+import rootReducer from "./reducers/rootReducer";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk, logger))
+);
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
+
+
+this is what they have on their latest commit, we could use this to avoid merge conflicts 
+k: awesome! I couldnt find it. Appreciate. I should be paying attention but lectures are boring haha lol agreed 
+k: how did you find their latest commit? did they create a rootReducer file?
+m: on the futrue-hope-client, under the feautre-router branch and yes they did let me look at it, I'll post it as a comment in the rootreducer file
+*/
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,9 +1,8 @@
 import { combineReducers } from "redux";
 import { meetings } from "./meetings";
 import { messaging } from "./messaging";
-import { signup } from "./signup";
-import { firestoreReducer } from "redux-firestore";
-import { firebaseReducer } from "react-redux-firebase";
+import { auth } from "./signup";
+
 
 export default combineReducers({
   meetings,
@@ -12,3 +11,31 @@ export default combineReducers({
   firebaseReducer, //handles auth syncing
   firestoreReducer //has all the data =D
 });
+// do you want to store these like firebase: firebaseReducer?
+
+/*
+
+import { firestoreReducer } from "redux-firestore";
+import { firebaseReducer } from "react-redux-firebase";
+
+import meetingsReducer from "./meetings";
+import messagingReducer from "./meetings";
+import authReducer from "./auth";
+
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+    Every reducer imported gets listed here!!
+  meetingsReducer,
+  messagingReducer,
+  authReducer
+});
+
+export default rootReducer;
+
+how do I pull and merge with my branch? I can just delete all this and recreate it
+oof i'im not sure. I know you can do it with master but i'm not sure how to with other branches. Maybe you could just run git merge {their branch name here} That sounds correct to me but if it breaks not my fault
+lol
+
+maybe just git merge feature.......whatever the branch name is? so switch to that branch. do a pull. then do a git merge with it?
+*/
