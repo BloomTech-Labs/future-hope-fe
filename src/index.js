c141fdd8a37c8fa12eeccbdb456310e0d2dfd416
LadyKerr <kadiekerr@gmail.com>
2019-08-19 12:44:37 -0400

    removed comments

--- src/index.js
+++ src/index.js
@@ -36,38 +36,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-
-/*
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
-import { createStore, applyMiddleware, compose } from "redux";
-import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-import logger from "redux-logger";
-
-import rootReducer from "./reducers/rootReducer";
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk, logger))
-);
-ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
-  document.getElementById("root")
-);
-
-
-this is what they have on their latest commit, we could use this to avoid merge conflicts 
-k: awesome! I couldnt find it. Appreciate. I should be paying attention but lectures are boring haha lol agreed 
-k: how did you find their latest commit? did they create a rootReducer file?
-m: on the futrue-hope-client, under the feautre-router branch and yes they did let me look at it, I'll post it as a comment in the rootreducer file
-*/
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -8,27 +8,3 @@ const rootReducer = combineReducers({
 });
 
 export default rootReducer;
-// do you want to store these like firebase: firebaseReducer? yes
-
-/*
-
-import meetingsReducer from "./meetings";
-import messagingReducer from "./meetings";
-import authReducer from "./auth";
-
-import { combineReducers } from "redux";
-
-const rootReducer = combineReducers({
-    Every reducer imported gets listed here!!
-  meetingsReducer,
-  messagingReducer,
-  authReducer
-});
-
-export default rootReducer;
-
-how do I pull and merge with my branch? I can just delete all this and recreate it
-oof i'im not sure. I know you can do it with master but i'm not sure how to with other branches. Maybe you could just run git merge {their branch name here} That sounds correct to me but if it breaks not my fault
-lol
-
-*/
