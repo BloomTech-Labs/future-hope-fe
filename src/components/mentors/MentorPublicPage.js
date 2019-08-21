5acbb8baa11d20dfab6a4d8a9582d50e839676ef
LadyKerr <kadiekerr@gmail.com>
2019-08-21 18:22:00 -0400

    centered dummyData cards on the screen

--- src/components/mentors/MentorPublicPage.js
+++ src/components/mentors/MentorPublicPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardActionArea from "@material-ui/core/CardActionArea";
@@ -11,7 +12,7 @@ import Typography from "@material-ui/core/Typography";
 const useStyles = makeStyles({
   card: {
     maxWidth: 345,
-    marginBottom: 40
+    marginBottom: 20
   },
   media: {
     height: 140
@@ -24,41 +25,51 @@ const MentorPublicPage = props => {
   console.log(mentors);
 
   return (
-    <Card className={classes.card}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt="profile picture"
-          className={classes.media}
-          image={mentors.photoUrl}
-          title="profile picture"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h3" align="left">
-            {mentors.name}
-          </Typography>
-          <Typography
-            gutterBottom
-            color="textSecondary"
-            variant="subtitle1"
-            align="left"
-          >
-            Location: {mentors.city}
-          </Typography>
-          <Typography paragraph variant="h5" align="left">
-            About Me: {mentors.about}
-          </Typography>
-          <Typography gutterBottom variant="h5" align="left">
-            Skills: {mentors.skills}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button href="#" size="small" color="secondary">
-          Contact {mentors.name}
-        </Button>
-      </CardActions>
-    </Card>
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justify="center"
+    >
+      <Grid>
+        <Card className={classes.card}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              alt="profile picture"
+              className={classes.media}
+              image={mentors.photoUrl}
+              title="profile picture"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h3" align="left">
+                {mentors.name}
+              </Typography>
+              <Typography
+                gutterBottom
+                color="textSecondary"
+                variant="subtitle1"
+                align="left"
+              >
+                Location: {mentors.city}
+              </Typography>
+              <Typography paragraph variant="h5" align="left">
+                About Me: {mentors.about}
+              </Typography>
+              <Typography gutterBottom variant="h5" align="left">
+                Skills: {mentors.skills}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button href="#" size="small" color="secondary">
+              Contact {mentors.name}
+            </Button>
+          </CardActions>
+        </Card>
+      </Grid>
+    </Grid>
   );
 };
 
