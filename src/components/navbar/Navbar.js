de54a2a019c38f7812371f6adb69540576485df7
Steve Barrett <steven122185@gmail.com>
2019-08-22 19:30:47 -0400

    Added the ability to change meeting days and created a framework for the edit form

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,6 +5,7 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
+import Calendar from '../calendar/Calendar';
 // import MenuIcon from "@material-ui/icons/Menu";
 // import { NavLink } from "react-router-dom";
 
@@ -19,6 +20,10 @@ const useStyles = makeStyles(theme => ({
     flexGrow: 1
   }
 }));
+var meetingTime = new Date();
+var meetingTime2 = new Date();
+meetingTime2.setDate(meetingTime.getDate() + 1);
+var events = [{title: 'Meeting', start: meetingTime}, {title: 'Meeting', start: meetingTime2}]
 
 const Navbar = () => {
   const classes = useStyles();
@@ -46,7 +51,8 @@ const Navbar = () => {
           <Button color="primary" href='/login'>Login</Button>
           <Button color="primary" href='/signup'>SignUp</Button>
         </Toolbar>
-      </AppBar>      
+      </AppBar>
+      <Calendar events={events} />      
     </div>
   );
 
