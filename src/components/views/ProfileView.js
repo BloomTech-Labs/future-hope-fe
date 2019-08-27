6c9531bdb6dad13b7696a21be82475f4b3e1afb8
micahjones13 <micahjones13@gmail.com>
2019-08-27 11:34:04 -0600

    added a bit of styling to profile view. WIP: add better styling, it still looks basic

--- src/components/views/ProfileView.js
+++ src/components/views/ProfileView.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Button from "@material-ui/core/Button";
+
+
+
+import {
+    MDBContainer
+  } from "mdbreact";
+
 
 
 const ProfileView = (props) => {
     
     
+    
     return (
-        <div>
-            <p>Name: {props.userInfo.fullName}</p>
-            <p>Location:{props.userInfo.city}, {props.userInfo.country}</p>
+       
+            <MDBContainer className = 'profile-container' fluid>
+                <div className = 'w-25 pr-5'>
+                    <img src = {props.userInfo.photoUrl} alt = 'profile' className = 'img-fluid rounded float-left z-depth-1-half' />
+                </div>
+                <div className = 'info-container'>
+                    <p className = 'left-aligned'>Name: {props.userInfo.fullName}</p>
+                    <p className = 'left-aligned'>Location:{props.userInfo.city}, {props.userInfo.country}</p>
 
-            <p>About me: {props.userInfo.aboutMe}</p>
-            <p>Account Type: {props.userInfo.userType}</p>
-
-        </div>
+                    <p className = 'left-aligned' >About me: {props.userInfo.aboutMe}</p>
+                    <p className = 'left-aligned'>Account Type: {props.userInfo.userType}</p>
+                    <Button
+                        size = 'small'
+                        color = 'primary'
+                        variant = 'contained'
+                    >
+                        Contact {props.userInfo.fullName}
+                    </Button>
+                </div>
+            </MDBContainer>
+       
     )
 }
 
