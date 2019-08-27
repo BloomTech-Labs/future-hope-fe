6d57384cb00f51b16e821d2a62e1f4d613d6a3e0
Eric Fugleberg <ericfugleberg@Erics-MacBook-Pro.local>
2019-08-27 18:44:39 -0500

    userarray is rendering

--- src/components/dashboard/AdminDashboard.js
+++ src/components/dashboard/AdminDashboard.js
@@ -2,18 +2,41 @@ import React, { Component } from "react";
 import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
 import { connect } from "react-redux";
 import Button from "@material-ui/core/Button";
-import { firestoreConnect } from "react-redux-firebase";
-import { compose } from "redux";
+import { firestore } from "../../config/fbConfig.js";
 //import { Redirect } from "react-router-dom";
 import "./Dashboard.css";
 import MentorTable from "./MentorTable.js";
 import TeacherTable from "./TeacherTable.js";
+// import { QuerySnapshot } from "@google-cloud/firestore";
 
 class AdminDashboard extends Component {
+  state = {
+    users: []
+  };
+
+  componentDidMount = async () => {
+    let userArray = [];
+    const userRef = firestore.collection("users");
+    const userList = await userRef.get().then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        console.log(doc.data());
+        userArray.push({
+          name: doc.data().fullName
+        });
+      });
+    });
+    this.setState({
+      users: userArray
+    });
+    console.log("useArray", userArray);
+  };
+
   render() {
     const { auth, userInfo } = this.props;
+
     console.log("auth", auth);
     console.log("userinfo", userInfo);
+
     //if (!auth.uid) return <Redirect to="/" />;
 
     return (
@@ -50,11 +73,9 @@ const mapStateToProps = state => {
   console.log(state);
   return {
     auth: state.firebase.auth,
+    users: state.firestore.ordered.users,
     userInfo: state.firebase.profile //need access to the users collection instead to check userType and render props in the tables
   };
 };
 
-export default compose(
-  connect(mapStateToProps),
-  firestoreConnect({ collection: "users", limit: 3 })
-)(AdminDashboard);
+export default connect(mapStateToProps)(AdminDashboard);
