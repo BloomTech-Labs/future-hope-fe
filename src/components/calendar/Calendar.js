82b659488ac976a5bc0a69988e8bd909c8f072f1
Joel Perez <joelness03@gmail.com>
2019-08-27 10:02:04 -0600

    added user to mapStateToProps, refactored user GET in CDM

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import interactionPlugin from '@fullcalendar/interaction'; // needed for dayClick
-import { MDBBtn } from 'mdbreact';
-import flatpickr from 'flatpickr';
-import './flatpickr.min.css';
-import './flatpickr.css';
-import swal from 'sweetalert';
-import { connect } from 'react-redux';
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction"; // needed for dayClick
+import { MDBBtn } from "mdbreact";
+import flatpickr from "flatpickr";
+import "./flatpickr.min.css";
+import "./flatpickr.css";
+import swal from "sweetalert";
+import { connect } from "react-redux";
 
-import { firestore, auth } from '../../config/fbConfig.js';
-import { userStore } from '../../actions/auth.js';
+import { firestore, auth } from "../../config/fbConfig.js";
+import { userStore } from "../../actions/auth.js";
 
-import '../auth/Login.scss';
-import './main.scss';
+import "../auth/Login.scss";
+import "./main.scss";
 
 class Calendar extends React.Component {
   calendarComponentRef = React.createRef();
   state = {
     calendarWeekends: true,
     displayPicker: true,
-    meetingParticipant: '',
+    meetingParticipant: "",
     events: []
     // Not sure if we need to keep user in local state
     // user: {
@@ -29,36 +29,38 @@ class Calendar extends React.Component {
   };
 
   componentDidMount = async () => {
-    // handles the reload of the page after a user has already logged in.
-    if (auth.currentUser.uid && !this.props.auth.user.uid) {
-      let userInfo = null;
-      try {
-        await firestore
-          .collection('users')
-          .doc(auth.currentUser.uid)
-          .onSnapshot(snapshot => {
-            console.log('userInfo', snapshot.data());
-            userInfo = snapshot.data();
-            this.props.userStore(userInfo);
-            // Not sure if we need to keep user in local state
-            // this.setState({
-            //   user: userInfo
-            // })
-          });
-      } catch (err) {
-        alert(err);
-      }
-    }
+    // if (auth.currentUser.uid && !this.props.auth.user.uid) {
+    //   let userInfo = null;
+    //   try {
+    //     await firestore
+    //       .collection("users")
+    //       .doc(auth.currentUser.uid)
+    //       .onSnapshot(snapshot => {
+    //         console.log("userInfo", snapshot.data());
+    //         userInfo = snapshot.data();
+    //         this.props.userStore(userInfo);
+    //         // Not sure if we need to keep user in local state
+    //         // this.setState({
+    //         //   user: userInfo
+    //         // })
+    //       });
+    //   } catch (err) {
+    //     alert(err);
+    //   }
+    // }
     // Search for meetings tied to UID
-    const uid = auth.currentUser.uid;
+    const uid = this.props.user.uid;
+    console.log(uid);
     let events = [];
-    const meetingsRef = await firestore.collection('meetings');
-    // finds all meetings docs with matching UID and pushes each to the events array and then sets array in state
+    const meetingsRef = await firestore.collection("meetings");
+    // finds all meeting docs with matching UID and pushes each to the events array and then sets array in state
     const userMeetings = await meetingsRef
-      .where('participantUIDs', 'array-contains', uid)
+      .where("participantUIDs", "array-contains", uid)
       .get()
       .then(querySnapshot => {
+        console.log(querySnapshot);
         querySnapshot.forEach(doc => {
+          console.log(doc.data());
           const meetingTime = doc.data().meetingTime.seconds * 1000;
           events.push({
             title: doc.data().title,
@@ -91,9 +93,9 @@ class Calendar extends React.Component {
           <FullCalendar
             defaultView='dayGridMonth'
             header={{
-              left: 'prev,next today',
-              center: 'title',
-              right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+              left: "prev,next today",
+              center: "title",
+              right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek"
             }}
             plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
             editable={true}
@@ -112,9 +114,9 @@ class Calendar extends React.Component {
 
   handleEventDrop = info => {
     swal({
-      title: 'Change Meeting Date?',
+      title: "Change Meeting Date?",
       text: `Meeting will be changed to ${info.event.start}`,
-      icon: 'warning',
+      icon: "warning",
       buttons: true,
       dangerMode: false
     }).then(changeDate => {
@@ -136,20 +138,20 @@ class Calendar extends React.Component {
           info.view.calendar.addEvent(e);
         });
         swal(`Meeting date has been changed to ${info.event.start}`, {
-          icon: 'success'
+          icon: "success"
         });
       } else {
-        swal('Cancelled, your meeting has not been changed!');
+        swal("Cancelled, your meeting has not been changed!");
       }
     });
   };
 
   handleEventClick = info => {
     swal({
-      text: 'Set Meeting Name',
-      content: 'input',
+      text: "Set Meeting Name",
+      content: "input",
       button: {
-        text: 'Submit!',
+        text: "Submit!",
         closeModal: true
       }
     }).then(name => {
@@ -173,20 +175,20 @@ class Calendar extends React.Component {
       });
 
       swal({
-        title: 'Would you like to change the date as well?',
+        title: "Would you like to change the date as well?",
         text: `Current date is ${info.event.start}`,
-        icon: 'warning',
+        icon: "warning",
         buttons: true,
         dangerMode: false
       }).then(changeDate => {
         if (changeDate) {
-          const myInput = document.querySelector('#futureButton');
+          const myInput = document.querySelector("#futureButton");
           const fp = flatpickr(myInput, {
-            position: 'below',
+            position: "below",
             enableTime: true,
             noCalendar: false,
-            dateFormat: 'H:i',
-            timeZone: 'local',
+            dateFormat: "H:i",
+            timeZone: "local",
             onClose: () => {
               // If the user canceled the picker the dates will be empty and there is nothing to do
               if (fp.selectedDates.length === 0) {
@@ -228,23 +230,23 @@ class Calendar extends React.Component {
   gotoPast = () => {
     let calendarApi = this.calendarComponentRef.current.getApi();
     // Displays a datetime picker to set a future meeting
-    const myInput = document.querySelector('#futureButton');
+    const myInput = document.querySelector("#futureButton");
     // Had to put the confirmation and state update in the onClose callback as execution continues even after the flatpickr is displayed
     const fp = flatpickr(myInput, {
-      position: 'below',
+      position: "below",
       enableTime: true,
       noCalendar: false,
-      dateFormat: 'H:i',
-      timeZone: 'local',
+      dateFormat: "H:i",
+      timeZone: "local",
       onClose: () => {
         // If the user canceled the picker the dates will be empty and there is nothing to do
         if (fp.selectedDates.length === 0) {
           return;
         }
         swal({
-          title: 'Schedule Meeting?',
-          text: 'Meeting will be added to the calendar!',
-          icon: 'warning',
+          title: "Schedule Meeting?",
+          text: "Meeting will be added to the calendar!",
+          icon: "warning",
           buttons: true,
           dangerMode: false
         }).then(scheduleAppointment => {
@@ -255,14 +257,14 @@ class Calendar extends React.Component {
               ...this.state,
               events: [
                 ...this.state.events,
-                { title: 'Meeting', start: fp.selectedDates[0] }
+                { title: "Meeting", start: fp.selectedDates[0] }
               ]
             });
-            swal('Meeting has been added to the calendar!', {
-              icon: 'success'
+            swal("Meeting has been added to the calendar!", {
+              icon: "success"
             });
           } else {
-            swal('Cancelled, your meeting has not been set!');
+            swal("Cancelled, your meeting has not been set!");
           }
         });
       }
@@ -273,18 +275,18 @@ class Calendar extends React.Component {
   handleDateClick = arg => {
     // Display only the time component of flatpickr so the user can select the meeting start time.  Like above the work is done in the
     // onClose function of flatickr as execution does not halt after the time picker is opened to allow the user to select a date.
-    const myInput = document.querySelector('#datepicker');
+    const myInput = document.querySelector("#datepicker");
     const fp = flatpickr(myInput, {
-      position: 'below',
+      position: "below",
       enableTime: true,
       noCalendar: true,
-      dateFormat: 'H:i',
-      timeZone: 'local',
+      dateFormat: "H:i",
+      timeZone: "local",
       onClose: () => {
         swal({
-          title: 'Schedule Meeting?',
-          text: 'Meeting will be added to the calendar!',
-          icon: 'warning',
+          title: "Schedule Meeting?",
+          text: "Meeting will be added to the calendar!",
+          icon: "warning",
           buttons: true,
           dangerMode: false
         }).then(scheduleAppointment => {
@@ -303,14 +305,14 @@ class Calendar extends React.Component {
               ...this.state,
               events: [
                 ...this.state.events,
-                { title: 'Meeting', start: meetingDate }
+                { title: "Meeting", start: meetingDate }
               ]
             });
-            swal('Meeting has been added to the calendar!', {
-              icon: 'success'
+            swal("Meeting has been added to the calendar!", {
+              icon: "success"
             });
           } else {
-            swal('Cancelled, your meeting has not been set!');
+            swal("Cancelled, your meeting has not been set!");
           }
         });
       }
@@ -321,7 +323,8 @@ class Calendar extends React.Component {
 
 const mapStateToProps = state => {
   return {
-    auth: state.auth
+    auth: state.auth,
+    user: state.firebase.profile
   };
 };
 
