126a06f7665b9482ab5ffdb67fe4f78c49cc7074
Eric Fugleberg <ericfugleberg@Erics-MacBook-Pro.local>
2019-08-28 11:33:53 -0500

    rendering dynamic data on mentor table

--- src/components/dashboard/MentorTable.js
+++ src/components/dashboard/MentorTable.js
@@ -5,41 +5,42 @@ const MentorTable = props => {
   const { users } = props;
   console.log("usersss", users);
 
-  return users.map(user => {
-    if (user.userType === "mentor") {
-      return (
-        <div>
-          <h2 className="table-heading">Pending Mentor Applications</h2>
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Names</th>
-                <th scope="col">Account Type</th>
-                <th scope="col">City</th>
-                <th scope="col">State/Province</th>
-                <th scope="col">View</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>{user.name}</td>
-                <td>{user.userType}</td>
-                <td>{user.city}</td>
-                <td>{user.stateProvince}</td>
-                <td>
-                  <Button>View</Button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      );
-    } else {
-      return null;
-    }
-  });
+  return (
+    <div>
+      <h2 className="table-heading">Pending Mentor Applications</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Names</th>
+            <th scope="col">Account Type</th>
+            <th scope="col">City</th>
+            <th scope="col">State/Province</th>
+            <th scope="col">View</th>
+          </tr>
+        </thead>
+        {users.map(user => {
+          if (user.userType === "mentor") {
+            return (
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>{user.name}</td>
+                  <td>{user.userType}</td>
+                  <td>{user.city}</td>
+                  <td>{user.stateProvince}</td>
+                  <td>
+                    <Button>View</Button>
+                  </td>
+                </tr>
+              </tbody>
+            );
+          } else {
+            return null;
+          }
+        })}
+      </table>
+    </div>
+  );
 };
 
 export default MentorTable;
