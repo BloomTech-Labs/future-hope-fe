ed0d8319ab79ca839c25130c4aaacb801f195664
George Kando <gkando@gmail.com>
2019-08-29 16:17:22 -0500

    initial commit for footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,79 @@
+/*eslint-disable*/
+import React from "react";
+import PropTypes from "prop-types";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+import { List, ListItem, withStyles } from "@material-ui/core";
+
+// @material-ui/icons
+import Favorite from "@material-ui/icons/Favorite";
+
+import footerStyle from "./footerStyle";
+
+const Footer = ({ ...props }) => {
+  const { classes, whiteFont } = props;
+  const footerClasses = classNames({
+    [classes.footer]: true,
+    [classes.footerWhiteFont]: whiteFont
+  });
+  const aClasses = classNames({
+    [classes.a]: true,
+    [classes.footerWhiteFont]: whiteFont
+  });
+  return (
+    <footer className={footerClasses}>
+      <div className={classes.container}>
+        <div className={classes.left}>
+          <List className={classes.list}>
+            <ListItem className={classes.inlineBlock}>
+              <a
+                href="#"
+                className={classes.block}
+                target="_blank"
+              >
+                Our Story
+              </a>
+            </ListItem>
+            <ListItem className={classes.inlineBlock}>
+              <a
+                href="#"
+                className={classes.block}
+                target="_blank"
+              >
+                About us
+              </a>
+            </ListItem>
+            <ListItem className={classes.inlineBlock}>
+              <a
+                href="#"
+                className={classes.block}
+                target="_blank"
+              >
+                Blog
+              </a>
+            </ListItem>
+          </List>
+        </div>
+        <div className={classes.right}>
+          &copy; {1900 + new Date().getYear()} , made with{" "}
+          <Favorite className={classes.icon} /> by{" "}
+          <a
+            href="#"
+            className={aClasses}
+            target="_blank"
+          >
+            Lambda School
+          </a>{" "}
+          for a better web & better world.
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+Footer.propTypes = {
+  classes: PropTypes.object.isRequired,
+  whiteFont: PropTypes.bool
+};
+
+export default withStyles(footerStyle)(Footer);
