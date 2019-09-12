c8484087d412e9e1c3b928dd11a3d592a976c07f
Steve Barrett <steven122185@gmail.com>
2019-09-12 18:35:07 -0400

    Can start a new conversation by searching for people and updated styling and bug fixes
    Co-authored-by: Joel Perez <joelness03@gmail.com>
    Co-authored-by: Micah Jones <micahjones13@gmail.com>

--- src/components/Messaging/Message.js
+++ src/components/Messaging/Message.js
@@ -1,26 +1,31 @@
-import React from "react";
-import Avatar from '@material-ui/core/Avatar';
+import React, { useEffect } from "react";
+import Avatar from "@material-ui/core/Avatar";
 import { makeStyles } from "@material-ui/core/styles";
-
 
 const useStyles = makeStyles({
   avatar: {
-    margin: 10,
+    margin: 10
   }
 });
 
 const Message = props => {
- const classes = useStyles();
-    return (
-      <div className='message-wrapper'>
-        {/* //! replace with props, add img for profile pic */}
-        {/*<Avatar alt="user avatar" src={`${props.message.avatar}`} className={classes.avatar} /> */}
-        <p>{props.message.content}</p>
-       
-      </div>
-    );
-  
+  const classes = useStyles();
+
+  return (
+    <div
+      className={`message-wrapper ${
+        props.message.sentBy === props.userInfo.uid ? "match" : "no-match"
+      }`}
+    >
+      {/* //! replace with props, add img for profile pic */}
+      {/*<Avatar alt="user avatar" src={`${props.message.avatar}`} className={classes.avatar} /> */}
+
+      <p>{props.message.content}</p>
+    </div>
+  );
 };
 
 export default Message;
 
+//With overflow: auto, a scroll bar is available with messages to check older ones. Do we need a limit if we have that
+//capability? Do we change limit on scroll up?
