d142e62d3db7d8d5d3f1fd0c7ec03a4ece337376
micahjones13 <micahjones13@gmail.com>
2019-09-16 15:51:06 -0600

    updated some styling on messages, added avatar, name, sentby

--- src/components/Messaging/Message.js
+++ src/components/Messaging/Message.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import { makeStyles } from "@material-ui/core/styles";
+import moment from "moment";
 
 const useStyles = makeStyles({
   avatar: {
@@ -10,17 +11,37 @@ const useStyles = makeStyles({
 
 const Message = props => {
   const classes = useStyles();
-
+  // const {timestamp} = props;
   return (
-    <div
-      className={`message-wrapper ${
-        props.message.sentBy === props.userInfo.uid ? "match" : "no-match"
-      }`}
-    >
-      {/* //! replace with props, add img for profile pic */}
-      {/*<Avatar alt="user avatar" src={`${props.message.avatar}`} className={classes.avatar} /> */}
-
-      <p>{props.message.content}</p>
+    <div className="message-container">
+      <div
+        className={`message-wrapper ${
+          props.message.sentBy === props.userInfo.uid ? "match" : "no-match"
+        }`}
+      >
+        <div className="message-sender">
+          <h6>{props.message.name}</h6>
+        </div>
+        <p>{props.message.content}</p>
+        <div>
+          <span className="sent-date">
+            Sent {moment(props.message.timestamp.toDate()).fromNow()}
+          </span>
+        </div>
+      </div>
+      <div
+        className={`message-avatar ${
+          props.message.sentBy === props.userInfo.uid
+            ? "avatar-right"
+            : "avatar-left"
+        }`}
+      >
+        <Avatar
+          alt="user avatar"
+          src={`${props.message.avatar}`}
+          className={classes.avatar}
+        />
+      </div>
     </div>
   );
 };
@@ -29,3 +50,31 @@ export default Message;
 
 //With overflow: auto, a scroll bar is available with messages to check older ones. Do we need a limit if we have that
 //capability? Do we change limit on scroll up?
+
+/*
+
+  <div className="message-container">
+      <div
+        className={`message-wrapper ${
+          props.message.sentBy === props.userInfo.uid ? "match" : "no-match"
+        }`}
+      >
+        <p>{props.message.content}</p>
+      </div>
+      <div
+        className={`message-avatar ${
+          props.message.sentBy === props.userInfo.uid ? "match" : "no-match"
+        }`}
+      >
+        <Avatar
+          alt="user avatar"
+          src={`${props.message.avatar}`}
+          className={classes.avatar}
+        />
+      </div>
+    </div>
+
+
+
+
+*/
