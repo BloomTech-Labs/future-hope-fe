92d5ddf19f83846ce71a72762b6e1a5d6cb87e65
Eric Fugleberg <ericfugleberg@Erics-MacBook-Pro.local>
2019-09-18 16:11:14 -0500

    added text to the button to awaiting approval component

--- src/components/views/AwaitingApproval.js
+++ src/components/views/AwaitingApproval.js
@@ -1,37 +1,40 @@
-import React from 'react';
-import { MDBContainer } from 'mdbreact';
-import Button from "@material-ui/core/Button";
-
+import React from "react";
+import { MDBContainer } from "mdbreact";
+import { Button } from "@material-ui/core";
 
 class AwaitingApproval extends React.Component {
-    //leaving this as a class component in case we do state stuff in the future
+  //leaving this as a class component in case we do state stuff in the future
 
-    pushToHomePage = () => {
-        this.props.history.push(`/`);
-      }
- 
-    render() {
-        return (
-            <MDBContainer fluid className = 'd-flex flex-column align-items-center' >
-                <h2 className = 'p-2'>Your application has been submitted!</h2>
-                <h4>Here's what happens next: </h4> 
-                <ul className = 'p-3'>
-                    <li className = 'p-1'>Your application has been sent to an administrator for approval.</li>
-                    <li className = 'p-1'>Once your application is accepted, you can start exploring the app!</li>
-                    <li className = 'p-1'>Please contanct CONTACTINFOHERE for further information regarding your application.</li>
+  pushToHomePage = () => {
+    this.props.history.push(`/`);
+  };
 
-                </ul>
-            <Button onClick={() => this.pushToHomePage()}></Button>
-
-            </MDBContainer>
-        );
-    }
+  render() {
+    return (
+      <MDBContainer fluid className="d-flex flex-column align-items-center">
+        <h2 className="p-2">Your application has been submitted!</h2>
+        <h4>Here's what happens next: </h4>
+        <ul className="p-3">
+          <li className="p-1">
+            Your application has been sent to an administrator for approval.
+          </li>
+          <li className="p-1">
+            Once your application is accepted, you can start exploring the app!
+          </li>
+          <li className="p-1">
+            Please contanct CONTACTINFOHERE for further information regarding
+            your application.
+          </li>
+        </ul>
+        <div>
+          <Button onClick={() => this.pushToHomePage()}>Return to HomePage</Button>
+        </div>
+      </MDBContainer>
+    );
+  }
 }
 
-
-
 export default AwaitingApproval;
-
 
 /*
 here is that progress bar I was using.
