60988ec0fd1e096a8ea68372a22c4bef38eec557
Joel Perez <joelness03@gmail.com>
2019-09-23 08:16:29 -0600

    added comments and cleaned up links in navbar

--- src/components/navbar/NavbarLinks.js
+++ src/components/navbar/NavbarLinks.js
@@ -1,14 +1,12 @@
 /*eslint-disable*/
 import React from "react";
-import { useSelector } from 'react-redux'
+import { useSelector } from "react-redux";
 // react components for routing our app without refresh
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
 import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import Tooltip from "@material-ui/core/Tooltip";
 
 // core components
 import Button from "../shared/components/Button";
@@ -17,42 +15,42 @@ import { navbarLinksStyle } from "./navbarStyle";
 
 function NavbarLinks({ ...props }) {
   const { classes } = props;
-  const links = props.config.links
-/*
+  const links = props.config.links;
+  /*
   Set auth to true only when isLoaded = true & isEmpty = false
   There is probaly a better way to approach this...
   But my skills and time are in short supply :-D */
-  const auth = useSelector(state => state.firebase.auth.isLoaded && !state.firebase.auth.isEmpty);
-/*
+  const auth = useSelector(
+    state => state.firebase.auth.isLoaded && !state.firebase.auth.isEmpty
+  );
+  /* 
   Filter Navbar Links
-  auth = true filter links (see navConfig),
-  auth = false render all links from config.*/
-  const navLink = (links) => {
-    if(auth){
-      return links.filter(link => link.auth === auth)
+ * auth = true filter links (see navConfig), show links while logged in or out
+ * auth = false render all links from config. Only Show these links when logged out
+ * */
+  const navLink = links => {
+    if (auth) {
+      return links.filter(link => link.auth === auth);
     } else {
-      return links
+      return links;
     }
-  }
+  };
 
   return (
     <List className={classes.list}>
-      {navLink(links)
-        .map(link => { return (
-          <Link 
-            id={link.href} 
+      {navLink(links).map(link => {
+        return (
+          <NavLink
+            id={link.href}
             className={classes.listItem}
             to={`${link.href}`}
           >
-            <Button
-              color="transparent"
-              className={classes.navLink}
-            >
+            <Button color="transparent" className={classes.navLink}>
               {link.text}
             </Button>
-          </Link> 
-        )})
-      }
+          </NavLink>
+        );
+      })}
     </List>
   );
 }
