2d35a46b66eaa78bdb424dceba83d9684fd529bc
Cesar Aponte <cesar.aponte.1996.vazquez@gmail.com>
2019-11-13 00:49:55 -0500

    have the new Training tab made, have dummy sub-categories semi-styled. working on importing collection from firebase back-end. will then add collapsion functionality and the ability to add new sub-categories

--- src/components/shared/components/Sidebar/sideBarItems.js
+++ src/components/shared/components/Sidebar/sideBarItems.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { firestore } from "../../../../config/fbConfig";
 
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
@@ -12,9 +13,19 @@ import CalendarTodayIcon from "@material-ui/icons/CalendarToday";
 import AccountBoxIcon from "@material-ui/icons/AccountBox";
 import LiveHelpIcon from "@material-ui/icons/LiveHelp";
 import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
+import SchoolIcon from "@material-ui/icons/School"
+import AddIcon from '@material-ui/icons/Add';
+import { Menu, MenuItem, MenuList } from '@material-ui/core';
+import "./sidebar.css"
+
+
 
 export const MainListItems = props => {
+
+    const [navItems, setNavItems] = useState([])
+
   return (
+    
     <List>
       <ListItem button component={Link} to={"/dashboard"}>
         <ListItemIcon>
@@ -68,6 +79,25 @@ export const MainListItems = props => {
         </ListItemIcon>
         <ListItemText primary="Update Profile" />
       </ListItem>
+      <List>
+        <ListItem button className="trainingBtn">
+          <ListItemIcon>
+            <SchoolIcon style={{ color: "#ff9800"}} />
+          </ListItemIcon>
+          <ListItemText primary="Training"/>
+        </ListItem>
+        <ListItemIcon button className="iconBox">
+            <AddIcon style={{ color: "#ff9800"}} />
+          </ListItemIcon>
+        <MenuList className="trainingCategories">
+          <MenuItem button  component={Link} to={"/food"} className="test"> 
+            Food
+          </MenuItem>
+          <MenuItem className="test">
+            Culture
+          </MenuItem>
+        </MenuList>
+      </List>
     </List>
   );
 };
