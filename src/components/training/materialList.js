5b355cc00492bf123a9136f7893e806d1b53dead
karenjli <karenjy.li@gmail.com>
2019-11-13 14:45:47 -0600

    added training card

---
+++ src/components/training/materialList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+//components
+import MaterialCard from "./materialCard";
+
+const MaterialList = () => {
+  return (
+    <div className="material-list">
+      <MaterialCard />
+    </div>
+  );
+};
+
+export default MaterialList;
