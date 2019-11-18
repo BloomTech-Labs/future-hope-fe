064f0bc839f240d4791ac5317ea3e35670d37994
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-11-18 14:51:24 -0600

    moved images file

--- src/components/Training/randomImages.js
+++
@@ -1,9 +0,0 @@
-const images = [
-        "https://3blaws.s3.amazonaws.com/images/DSC06369.JPG",
-        "https://radiantghanaguide.files.wordpress.com/2017/10/rural-life-in-ghana-e1508255916440.jpg",
-        "https://cocoainitiative.org/wp-content/uploads/2017/12/Abease_june_2017DSC_0131.jpg"
-]
-
-const photosGhana = images[Math.floor(Math.random()* images.length)] 
-
-export default photosGhana;
