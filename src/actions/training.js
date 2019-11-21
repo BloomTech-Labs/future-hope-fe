4ff1dd7e962462e81b9245d0cecc26a65a4052c5
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-11-21 12:05:21 -0500

    commit to pull

--- src/actions/training.js
+++ src/actions/training.js
@@ -1,22 +1,19 @@
-import axios from "axios";
+import { firestore } from "../config/fbConfig"
 
 // action types
 export const FETCH_TRAINING_CREATE_START = "TRAINING_CREATE_START"
-export const FETCH_TRAINING_CREATE_SUCCESS = "TRAINING_CREATE_SUCCESS";
-export const FETCH_TRAINING_CREATE_FAILURE = "TRAINING_CREATE_FAILURE";
+export const FETCH_TRAINING_CREATE_SUCCESS = "TRAINING_CREATE_SUCCESS"
+export const FETCH_TRAINING_CREATE_FAILURE = "TRAINING_CREATE_FAILURE"
 
 // action creators
-export const getTraining = () => dispatch => {
-
-    dispatch({ type: FETCH_TRAINING_CREATE_START })
-    axios
-        .get(`http://localhost:3000/dummy-training`)
-        .then(res => {
-            console.log("axios request: ", res.data)
-            dispatch({ type: FETCH_TRAINING_CREATE_SUCCESS, payload: res.data });
-        })
-        .catch(error => {
-            console.log("Error, please try again.", error.res);
-            dispatch({ type: FETCH_TRAINING_CREATE_FAILURE, payload: error.res});
-        });
-};
+export const getTraining = () => async dispatch => {
+  dispatch({ type: FETCH_TRAINING_CREATE_START })
+  try {
+    let tabs = await firestore.collection("trainingTabNav").get()
+    console.log("TRAINING TABS: ", tabs)
+    dispatch({ type: FETCH_TRAINING_CREATE_SUCCESS, payload: tabs })
+  } catch (error) {
+    console.log("Error, please try again.", error.res)
+    dispatch({ type: FETCH_TRAINING_CREATE_FAILURE, payload: error.res })
+  }
+}
