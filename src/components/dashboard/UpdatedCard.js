7d9dc005505aba1cf021916f8c266c9deb10cab2
karenjli <karenjy.li@gmail.com>
2019-12-03 12:58:41 -0600

    Training material card refactored w/ Material UI

---
+++ src/components/dashboard/UpdatedCard.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+import photosGhana from "../dashboard/randomImages";
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345
+  },
+  media: {
+    height: 140
+  }
+});
+
+export default function MediaCard() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Edit
+        </Button>
+        <Button size="small" color="secondary">
+          Delete
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+// 12-3-2019
+// Payload into the card is unknown, thus hooks are not set up yet. Hooks are
+// expected to be used in this card. Data might be rendered on parent component and
+// passed down as props. See UpdateList.js component
+// Title, Description, URL, and Picture needed
