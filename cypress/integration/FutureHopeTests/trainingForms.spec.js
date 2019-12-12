db755209d57a88c563cd3ece1227636a9f53dad1
Diana Grams <deegrams221@gmail.com>
2019-12-12 13:21:17 -0700

    starting on edit materials unit testing

--- cypress/integration/FutureHopeTests/trainingForms.spec.js
+++ cypress/integration/FutureHopeTests/trainingForms.spec.js
@@ -60,3 +60,61 @@ context('Form submission', () => {
     })
   })
 })
+
+/*------------------------------------------------------------*/
+// Testing for Edit Materials Form
+describe('Edit materials form', () => {
+  beforeEach(() => {
+    cy.server()
+  })
+  
+  // enter heading accepts input
+  it('enter heading accepts input', () => {
+    const typedText = 'The Ocean'
+
+    cy.visit('http://localhost:3000/edit-materials')
+          
+    cy.get('.form-control').first()
+      .type(typedText)
+      .should('have.value', typedText)
+  })
+    
+  // enter description accepts input
+  it('enter description accepts input', () => {
+    const typedText = 'The Ocean is vast.'
+  
+    cy.visit('http://localhost:3000/edit-materials')
+    
+    cy.get('.form-control').eq(1)
+      .type(typedText)
+      .should('have.value', typedText)
+  })
+  
+  // enter URL accepts input
+  it('enter URL accepts input', () => {
+    const typedText = 'ocean.com'
+      
+    cy.visit('http://localhost:3000/edit-materials')
+    
+    cy.get('.form-control').eq(1)
+      .type(typedText)
+      .should('have.value', typedText)
+  })
+})
+  
+// Form submission
+context('Form submission', () => {
+  beforeEach(() => {
+    cy.server()
+  })
+    
+  // Adds a new material
+  it('Adds a new material', () => {
+  const itemText = 'The Ocean'
+    cy.route('POST', '/training/science', {
+      name: itemText,
+      id: 1,
+      isComplete: false
+    })
+  })
+})
