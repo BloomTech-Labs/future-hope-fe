c71b2fc3e281a716971d53868e77c38b65cbb369
Josiah <reikiryo@yahoo.com>
2020-05-06 13:17:01 -0400

    Adds new footer component for mobile usage and selectively renders footer based on screen size

--- src/components/footer/styles.js
+++ src/components/footer/styles.js
@@ -1,6 +1,6 @@
 import { container, dangerColor } from "../shared/styles/sharedStyle";
 
-const footerStyle = {
+export const mobileFooterStyle = {
   block: {
     color: "inherit",
     padding: "0.9375rem",
@@ -50,4 +50,62 @@ const footerStyle = {
     top: "3px"
   }
 };
-export default footerStyle;
+
+export const footerStyle = {
+  block: {
+    color: "inherit",
+    padding: "0.9375rem",
+    fontWeight: "500",
+    fontSize: "12px",
+    textTransform: "uppercase",
+    borderRadius: "3px",
+    textDecoration: "none",
+    position: "relative",
+    display: "block"
+  },
+  left: {
+    float: "left!important",
+    display: "block"
+  },
+  right: {
+    padding: "15px 0",
+    margin: "0",
+    float: "right!important"
+  },
+  footer: {
+    padding: "0.9375rem 0",
+    textAlign: "center",
+    display: "flex",
+    zIndex: "2",
+    position: "relative",
+    bottom: "0",
+    width: "100%"
+  },
+  a: {
+    color: dangerColor,
+    textDecoration: "none",
+    backgroundColor: "transparent"
+  },
+  footerWhiteFont: {
+    "&,&:hover,&:focus": {
+      color: "#FFFFFF"
+    }
+  },
+  container,
+  list: {
+    marginBottom: "0",
+    padding: "0",
+    marginTop: "0"
+  },
+  inlineBlock: {
+    display: "inline-block",
+    padding: "0px",
+    width: "auto"
+  },
+  icon: {
+    width: "18px",
+    color: dangerColor,
+    height: "18px",
+    top: "3px"
+  }
+};
